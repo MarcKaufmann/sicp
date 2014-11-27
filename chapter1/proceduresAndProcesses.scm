0aea1eb01a1f611413dd99d56d3728f3c0a9b7e1
Marc Kaufmann <marc.kaufmannmk@gmail.com>
2014-11-26 21:45:19 -0500

    Finish section 1.2.

--- chapter1/proceduresAndProcesses.scm
+++ chapter1/proceduresAndProcesses.scm
@@ -69,3 +69,81 @@
 ;; TODO: Exercise 1.13: Prove that the nth Fibonacci number is the closest integer to phi^n/sqrt(5), phi the golden ratio.
 
 ;; TODO: Write tests for all the exercises. I have not done so yet.
+
+;; Exercise 1.15
+;; a) It is applied as many times as we have to divide 12.15 by 3 to go below 0.1, i.e. five times.
+;; b) log(a)
+
+;; Exercise 1.16
+;; Write an iterative procedure for fast exponentiation.
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (fast-exp b n)
+  (define (iter b n acc)
+    (cond ((= n 0) acc)
+          ((even? n) (iter (* b b) (/ n 2) acc))
+          (else (iter b (- n 1) (* acc b)))))
+  (iter b n 1))
+
+;; Exercise 1.17 and 1.18 (I don't bother with 1.17 actually, don't do the recursive.)
+;; Write an iterative procedure for fast multiplication, based on addition, double and halve only.
+;; Obviously this requires those to exist, so I use multiplication for those.
+;; In binary, this would simply be a bitshift.
+
+(define (double n)
+  (* n 2))
+
+(define (halve n)
+  (/ n 2))
+
+(define (mult a b)
+  (define (iter a b acc)
+    (cond ((= b 0) acc)
+          ((even? b) (iter (double a) (halve b) acc))
+          (else (iter a (- b 1) (+ acc a)))))
+  (iter a b 0))
+
+(define (smallest-divisor n)
+  (define (divides? a b)
+    (= (remainder b a) 0))
+  (define (find-divisor test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor (+ test-divisor 2)))))
+  (if (divides? 2 n)
+      2
+      (find-divisor 3)))
+
+;; Exercise 1.22
+
+(define (prime? n)
+  (= (smallest-divisor n) n))
+
+(define (timed-prime-test n number-found)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime) number-found))
+
+(define (start-prime-test n start-time number-found)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time) (+ n 2) (+ number-found 1))
+      (timed-prime-test (+ n 2) number-found)))
+
+(define (report-prime elapsed-time n number-found)
+  (display " *** ")
+  (display elapsed-time)
+  (if (< number-found 3)
+      (timed-prime-test n number-found)
+      (display " Done ")))
+
+(define (find-three-primes start)
+  (if (even? start)
+      (timed-prime-test (+ start 1) 0)
+      (timed-prime-test start 0)))
+
+;; Exercise 1.25.
+;; Answer: No, that would almost surely be bad. a^p can get hellishly large if we don't take remainder wrt p and most numbers will not fit into memory any more. E.g. p roughly 10^10 gives us that a random number a will lead to (10^10)^(10^10), which is ridiculous! This number has 10^11 zeroes, ie is bigger than 2^32, and that's for primes of the order of billions. Go into billions of billions and you fill all the universe with zeroes rather quickly.
+
+

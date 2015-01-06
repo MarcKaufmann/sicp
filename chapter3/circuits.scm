ade13e73a6d32372deac8b3a73dc533a0a2d5ccf
marc <marc.kaufmannmk@gmail.com>
2015-01-06 11:53:28 -0500

    Finish circuits, ignore constraints.

--- chapter3/circuits.scm
+++ chapter3/circuits.scm
@@ -1,3 +1,45 @@
+(define (make-queue) 
+  (cons '() '()))
+
+(define (empty-queue? q)
+  (null? (front-ptr q)))
+	  
+(define (front-ptr q) (car q))
+(define (rear-ptr q) (cdr q))
+
+;; Is this really a good way of doing it? 
+;; Shouldn't front-ptr be replaced by front, and extract the element itself?
+;; Ah, no, it shouldn't. That's the job of this function. front-ptr always exists.
+;; front-queue itself can be replaced by a different implementation
+(define (front-queue q)
+  (if (empty-queue? q)
+	  (error "Trying to extract front of an empty queue -- FRONT-QUEUE" q)
+	  (car (front-ptr q))))
+
+(define (set-front-ptr! q new-pair)
+  (set-car! q new-pair))
+
+(define (set-rear-ptr! q new-pair)
+  (set-cdr! q new-pair))
+
+(define (insert-queue! q item)
+  (let ((new-pair (cons item '())))
+	(cond ((empty-queue? q)
+		   (set-front-ptr! q new-pair)
+		   (set-rear-ptr! q new-pair)
+		   q)
+		  (else 
+		   (set-cdr! (rear-ptr q) new-pair)
+		   (set-rear-ptr! q new-pair)
+		   q))))
+
+(define (delete-queue! q)
+  (cond ((empty-queue? q)
+		 (error "Trying to delete element from an empty queue -- DELETE-QUEUE" q))
+		(else
+		 (set-front-ptr! q (cdr (front-ptr q)))
+		 q)))
+
 (define (make-table)
   (list '*table*))
 
@@ -55,7 +97,7 @@
 
 (define (inverter input output)
   (define (invert)
-	(let ((new-value (logical-not input)))
+	(let ((new-value (logical-not (get-signal input))))
 	  (after-delay inverter-delay
 				   (lambda ()
 					 (set-signal! output new-value)))))
@@ -63,9 +105,9 @@
   'ok)
 
 (define (logical-not s)
-  (con ((= s 0) 1)
-	   ((= s 1) 0)
-	   (else (error "Invalid signal -- LOGICAL-NOT" s))))
+  (cond ((= s 0) 1)
+		((= s 1) 0)
+		(else (error "Invalid signal -- LOGICAL-NOT" s))))
 
 (define (and-gate a1 a2 output)
   (define (and-action-procedure)
@@ -138,3 +180,135 @@
 		(full-adder (car a_n) (car b_n) c-in (car s_k) c-out)
 		(iter-ripple-carry (cdr a_k b_k s_k c-in)))))
 	(iter-ripple-carry a_n b_n s_n c))
+
+(define (make-wire)
+  (let ((signal-value 0) (action-procedures '()))
+	(define (set-signal! new-value)
+	  (if (= signal-value new-value)
+		  'done
+		  (begin (set! signal-value new-value)
+				 (call-each action-procedures))))
+	(define (accept-action-procedure! proc)
+	  (set! action-procedures (cons proc action-procedures))
+	  (proc)
+	  'action-added)
+	(define (dispatch m)
+	  (cond ((eq? m 'get-signal) signal-value)
+			((eq? m 'set-signal!) set-signal!)
+			((eq? m 'add-action!) accept-action-procedure!)
+			(else (error "Unknown operation -- WIRE" m))))
+	dispatch))
+
+(define (call-each procedures)
+  (if (null? procedures)
+	  'done
+	  (begin
+		((car procedures))
+		(call-each (cdr procedures)))))
+
+(define (get-signal wire)
+  (wire 'get-signal))
+
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+				  action
+				  the-agenda))
+
+(define (propagate the-agenda)
+  (if (empty-agenda? the-agenda)
+	  'done
+	  (let ((first-item (first-agenda-item the-agenda)))
+		(first-item)
+		(remove-first-agenda-item! the-agenda)
+		(propagate the-agenda))))
+
+(define (probe name wire)
+  (add-action! wire
+			   (lambda ()
+				 (newline)
+				 (display name)
+				 (display " ")
+				 (display (current-time the-agenda))
+				 (display " New-value = ")
+				 (display (get-signal wire)))))
+
+(define (make-time-segment time queue)
+  (cons time queue))
+(define (segment-time s) (car s))
+(define (segment-queue s) (cdr s))
+
+(define (make-agenda) (list 0))
+(define (current-time agenda) (car agenda))
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+(define (segments agenda) (cdr agenda))
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+(define (first-segment agenda) (car (segments agenda)))
+(define (rest-segments agenda) (cdr (segments agenda)))
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+	(or (null? segments)
+		(< time (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+	(let ((q (make-queue)))
+	  (insert-queue! q action)
+	  (make-time-segment time q)))
+  (define (add-to-segments! segments)
+	(if (= (segment-time (car segments)) time)
+	  (insert-queue! (segment-queue (car segments))
+					 action)
+	  (let ((rest (cdr segments)))
+		(if (belongs-before? rest)
+		    (set-cdr! segments
+					  (cons (make-new-time-segment time action)
+							(cdr segments)))
+			(add-to-segments! rest)))))
+  (let ((segments (segments agenda)))
+	(if (belongs-before? segments)
+	  (set-segments! agenda
+					 (cons (make-new-time-segment time action)
+						   segments))
+	  (add-to-segments! segments))))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+	(delete-queue! q)
+	(if (empty-queue? q)
+	    (set-segments! agenda (rest-segments agenda)))))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+	  (error "Agenda is empty -- FIRST-AGENDA-ITEM")
+	  (let ((first-seg (first-segment agenda)))
+		(set-current-time! agenda (segment-time first-seg))
+		(front-queue (segment-queue first-seg)))))
+
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define input1 (make-wire))
+(define input2 (make-wire))
+(define sum (make-wire))
+(define carry (make-wire))
+(probe 'sum sum)
+(probe 'carry carry)
+
+(half-adder input1 input2 sum carry)
+(set-signal! input1 1)
+(propagate the-agenda)
+(set-signal! input2 1)
+(propagate the-agenda)
